1e3ca7a166f87b90a4b586fbeaa229a961b528ae
oreuis <nabi@intmail.pl>
2021-05-17 20:44:15 +0200

    Add new files

---
+++ WebApplication1/Controllers/NController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class NController : Controller
+    {
+        // GET: N
+        public ActionResult Index()
+        {
+            return View(new Class1()); 
+        }
+
+
+        [HttpPost]
+
+        public ActionResult Index(Class1 c,string wykonaj)
+        {
+            if(wykonaj == "add")
+            {
+                c.trzy = c.jeden + c.dwa;
+            }
+
+            else if (wykonaj == "min")
+            {
+                c.trzy = c.jeden - c.dwa;
+            }
+
+            return View(c);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Class1
+    {
+        public int jeden { get; set; }
+
+        public int dwa { get; set; }
+
+        public int trzy { get; set; }
+
+    }
+}
